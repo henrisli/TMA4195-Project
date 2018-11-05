d92a73f5707a9d67b1ad909f736339cce0c0a882
henrisli <henrisli@stud.ntnu.no>
2018-11-05 15:46:53 +0100

    Oppg 16

--- TMA4195_Project.py
+++ TMA4195_Project.py
@@ -4,16 +4,17 @@ import matplotlib.pyplot as plt
 
 # Import schemes:
 from upw import upw
+from god import god
 
 # Height equation flux function
 H = 50
-L = 1000
+L = 2000
 Q = 3/(365*24*3600)
 mu = 9.3e-25
 m = 3
 rho = 1000
 g = 9.81
-alpha = 5*np.pi/180
+alpha = 45*np.pi/180
 Theta = rho*g*H*np.sin(alpha)
 def flux(h):
     return 2*H**2/(Q*L)*mu*Theta**m*np.power(h,m+2)/(m+2)
@@ -23,16 +24,30 @@ def flux(h):
 # The following imports a function for the boundary conditions
 def inflow(h, n=0):
     if n == 0:
-        h[0] = H
+        h[0] = 0
     else:
-        h[0:n] = H
+        h[0:n] = 0
     return h
+
+# The following computes the production q, given a height profile h
+def production(h):
+    n = len(h) - 2
+    q = np.zeros(n + 2)
+    for i in range(n + 2):
+        if h[i]>0:
+            if i < n/3 + 1:
+                q[i] = 1
+            #elif i < 2*n/3 + 2:
+            #    q[i] = 1-(i-(n/3+1))/(n/6)
+            else:
+                q[i] = 1-(i-(n/3+1))/(n/6)    
+    
+    return q*1e+03
 
 # Solution of equation for height of glacier, both with classical
 # and Godunov schemes..
 def h_solution(method, T):
-    q = np.repeat(1,51)
-    q = np.append(q,np.linspace(1,-1,101))
+    #q = np.append(q,np.linspace(1,-1,101))
     #q = np.append(q,np.repeat(0,51))*Q
     #Here we compute the maximum value of f'(u).
     s = np.linspace(0,50,1001)
@@ -48,15 +63,19 @@ def h_solution(method, T):
     if method == 'classical':
         # Coarser grid
         x  = np.arange(-0.5*dx,L+1.5*dx,dx)
-        h0 = np.ones(len(x))*H
+        #h0 = np.ones(len(x))*H
+        h0 = np.ones(51)*H
+        h0 = np.append(h0,np.zeros(101))
         
         # Compute solutions with the three classical schemes
-        hu = upw(h0, 0.995, dx, T, flux, df, inflow, q)
+        hu = upw(h0, 0.995, dx, T, flux, df, inflow, production)
+        hu2 = upw(h0,0.995, dx, T*10, flux, df, inflow, production)
         # Plot results
         plt.figure()
         # Analytical solution:
         #plt.plot(xr, analytical(xr, T), color = 'red')
-        plt.plot(x[1:-1], hu[1:-1], '.', markersize = 3) # We dont want to plot fictitious nodes, thereby the command [1:-1].
+        plt.plot(x[1:-1], hu[1:-1], '-', markersize = 3) # We dont want to plot fictitious nodes, thereby the command [1:-1].
+        plt.plot(x[1:-1], hu2[1:-1], '-', markersize = 3)
         #plt.plot(x[1:-1], np.repeat(100,len(hu[1:-1])))
         plt.title("Upwind")
         # The following commented out section saves the plots
@@ -98,4 +117,4 @@ def h_solution(method, T):
         """
 
 
-h_solution('classical', 0.5)
+h_solution('classical', 10000)
--- upw.py
+++ upw.py
@@ -1,25 +1,27 @@
 
 import numpy as np
+import matplotlib.pyplot as plt
 
-
-def upw(h0, cfl, dx, T, flux, df, boundary,q):
+def upw(h0, cfl, dx, T, flux, df, boundary, production):
     h = np.copy(h0)
     t = 0.0
     dt = cfl*dx/max(abs(df(h0)))
     print(dt)
     print(dx)
     i = np.arange(1,len(h0)-1,1)
-    j = 1
-    while t<T:
-        if t+dt > T:
-            dt = T-t
+    j = 0
+    for k in range(T):
+        #if t+dt > T:
+        #    dt = T-t
         t += dt
         j += 1
         h = boundary(h)  
         f = flux(h)
-        print(f)
+        q = production(h)
+        #print(f)
         h[i] = h[i] - dt/dx*(f[i]-f[i-1]) + dt*q[i]
-        print(h)
+        #print(h)
         dt = cfl*dx/max(abs(df(h)))
     print(j)
+    print(t)
     return h
