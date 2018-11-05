0b8560133b09b3c5d5b8854a7774afdd4cb6c272
henrisli <henrisli@stud.ntnu.no>
2018-11-05 12:07:00 +0100

    Minor changes

--- TMA4195_Project.py
+++ TMA4195_Project.py
@@ -8,7 +8,7 @@ from upw import upw
 # Height equation flux function
 H = 50
 L = 1000
-Q = 7.5/(365*24*3600)
+Q = 3/(365*24*3600)
 mu = 9.3e-25
 m = 3
 rho = 1000
@@ -17,42 +17,47 @@ alpha = 5*np.pi/180
 Theta = rho*g*H*np.sin(alpha)
 def flux(h):
     return 2*H**2/(Q*L)*mu*Theta**m*np.power(h,m+2)/(m+2)
+
 #from analytical import analytical
 
 # The following imports a function for the boundary conditions
-from inflow import inflow
-
+def inflow(h, n=0):
+    if n == 0:
+        h[0] = H
+    else:
+        h[0:n] = H
+    return h
 
 # Solution of equation for height of glacier, both with classical
 # and Godunov schemes..
 def h_solution(method, T):
     q = np.repeat(1,51)
-    q = np.append(q,np.linspace(1,-1,50))
-    q = np.append(q,np.repeat(0,51))*Q
+    q = np.append(q,np.linspace(1,-1,101))
+    #q = np.append(q,np.repeat(0,51))*Q
     #Here we compute the maximum value of f'(u).
-    s = np.linspace(0,1,1001)
+    s = np.linspace(0,50,1001)
     dfv = max(np.diff(flux(s))/np.diff(s))
+    print(dfv)
     df = lambda u: np.zeros(len(u)) + dfv
     
         
     # Solutions on coarser grids
     N  = 150
-    dx = 1/N
+    dx = L/N
     
     if method == 'classical':
         # Coarser grid
-        x  = np.arange(-0.5*dx,1+1.5*dx,dx)
-        h0 = np.ones(len(x))
+        x  = np.arange(-0.5*dx,L+1.5*dx,dx)
+        h0 = np.ones(len(x))*H
         
         # Compute solutions with the three classical schemes
         hu = upw(h0, 0.995, dx, T, flux, df, inflow, q)
-                
-        
         # Plot results
         plt.figure()
         # Analytical solution:
         #plt.plot(xr, analytical(xr, T), color = 'red')
         plt.plot(x[1:-1], hu[1:-1], '.', markersize = 3) # We dont want to plot fictitious nodes, thereby the command [1:-1].
+        #plt.plot(x[1:-1], np.repeat(100,len(hu[1:-1])))
         plt.title("Upwind")
         # The following commented out section saves the plots
         """
@@ -93,4 +98,4 @@ def h_solution(method, T):
         """
 
 
-h_solution('classical', 10000)
+h_solution('classical', 0.5)
--- upw.py
+++ upw.py
@@ -6,13 +6,20 @@ def upw(h0, cfl, dx, T, flux, df, boundary,q):
     h = np.copy(h0)
     t = 0.0
     dt = cfl*dx/max(abs(df(h0)))
+    print(dt)
+    print(dx)
     i = np.arange(1,len(h0)-1,1)
+    j = 1
     while t<T:
         if t+dt > T:
             dt = T-t
         t += dt
-        h = boundary(h)
+        j += 1
+        h = boundary(h)  
         f = flux(h)
-        h[i] = h[i] - dt/dx*(f[i]-f[i-1])+dt*q[i]
+        print(f)
+        h[i] = h[i] - dt/dx*(f[i]-f[i-1]) + dt*q[i]
+        print(h)
         dt = cfl*dx/max(abs(df(h)))
+    print(j)
     return h
